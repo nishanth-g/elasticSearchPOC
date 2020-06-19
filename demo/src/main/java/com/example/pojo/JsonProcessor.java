79da3d46ed253117842d6361a11524c209b193dd
nishanth-g <67145366+nishanth-g@users.noreply.github.com>
2020-06-19 15:14:45 +0530

    elasticSearch POC changes

---
+++ demo/src/main/java/com/example/pojo/JsonProcessor.java
@@ -0,0 +1,19 @@
+package com.example.pojo;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+public class JsonProcessor { // This implementation can change as you want.
+
+    public static <T> List<T> unmarshallToList(String json, Class<T> classType)
+            throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        CollectionType javaType = mapper.getTypeFactory() // Check here whether you can take different function to directly de-serialize to the object than to a List.
+                .constructCollectionType(List.class, classType);
+
+        return mapper.readValue(json, javaType);
+    }
+}
---
+++ demo/src/main/java/com/example/pojo/SamplePojo.java
@@ -0,0 +1,60 @@
+package com.example.pojo;
+
+public class SamplePojo {
+
+	String employer;
+	String technology;
+	String published_date;
+	String executed_by;
+	/**
+	 * @return the employer
+	 */
+	public String getEmployer() {
+		return employer;
+	}
+	/**
+	 * @param employer the employer to set
+	 */
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+	/**
+	 * @return the technology
+	 */
+	public String getTechnology() {
+		return technology;
+	}
+	/**
+	 * @param technology the technology to set
+	 */
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+	/**
+	 * @return the published_date
+	 */
+	public String getPublished_date() {
+		return published_date;
+	}
+	/**
+	 * @param published_date the published_date to set
+	 */
+	public void setPublished_date(String published_date) {
+		this.published_date = published_date;
+	}
+	/**
+	 * @return the executed_by
+	 */
+	public String getExecuted_by() {
+		return executed_by;
+	}
+	/**
+	 * @param executed_by the executed_by to set
+	 */
+	public void setExecuted_by(String executed_by) {
+		this.executed_by = executed_by;
+	}
+	
+	
+	
+}
